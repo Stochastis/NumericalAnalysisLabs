7ab23567f30809ab50ac46151df5557c6e17a230
Stochastis <59930635+Stochastis@users.noreply.github.com>
2022-01-21 09:50:58 -0600

    Minor changes.

--- Program1-BisectionMethod.py
+++ Program1-BisectionMethod.py
@@ -1,6 +1,8 @@
+import math
+
 def f(x):
     # Change this return command to your desired function
-    return (x**3 - (9*x))
+    return (x**5 - 3*x + 5)
 
 
 def bisection(a, b, e, n):
@@ -32,34 +34,34 @@ def bisection(a, b, e, n):
     return
 
 
-print("Test 1.a.")
-print("Estimated p = " + str(bisection(1, 2, 0.001, 100)))
+# print("Test 1.a.")
+# print("Estimated p = " + str(bisection(1, 2, 0.001, 100)))
 
-print("Test 1.b.")
-print("Estimated p = " + str(bisection(-1, 4, 0.001, 100)))
+# print("Test 1.b.")
+# print("Estimated p = " + str(bisection(-1, 4, 0.001, 100)))
 
-print("Test 1.c.")
-print("Estimated p = " + str(bisection(-0.5, 1, 0.001, 8)))
+# print("Test 1.c.")
+# print("Estimated p = " + str(bisection(-0.5, 1, 0.001, 8)))
 
-print("Test 2.a.")
-print("Estimated p = " + str(bisection(2.7, 3.5, 0.001, 100)))
+# print("Test 2.a.")
+# print("Estimated p = " + str(bisection(2.7, 3.5, 0.001, 100)))
 
-print("Test 2.b.")
-print("Estimated p = " + str(bisection(-4, -1, 0.001, 20)))
+# print("Test 2.b.")
+# print("Estimated p = " + str(bisection(-4, -1, 0.001, 20)))
 
 while (True):
-    a = float(input("Enter a value for a."))
-    b = float(input("Enter a value for b."))
+    a = float(input("Enter a value for a. "))
+    b = float(input("Enter a value for b. "))
     while (a == b):
         print("Please make sure that a and b are different.")
-        a = float(input("Enter a value for a."))
-        b = float(input("Enter a value for b."))
+        a = float(input("Enter a value for a. "))
+        b = float(input("Enter a value for b. "))
 
-    e = float(input("Enter a value for the tolerance."))
+    e = float(input("Enter a value for the tolerance. "))
     while (e <= 0):
-        e = float(input("Please enter a positive value for the tolerance."))
+        e = float(input("Please enter a positive value for the tolerance. "))
 
-    n = int(input("Enter a max number of iterations."))
+    n = int(input("Enter a max number of iterations. "))
     while (n <= 0):
-        int(input("Please enter a positive value for the maximum number of iterations."))
+        int(input("Please enter a positive value for the maximum number of iterations. "))
     print("Estimated p = " + str(bisection(a, b, e, n)))
