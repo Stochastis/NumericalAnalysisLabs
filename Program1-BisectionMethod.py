a9e03d5e69d6c8882d89af7edad273d31a98c23b
Stochastis <59930635+Stochastis@users.noreply.github.com>
2022-01-25 19:37:44 -0600

    Function Intersect Modification

--- Program1-BisectionMethod.py
+++ Program1-BisectionMethod.py
@@ -2,52 +2,34 @@ import math
 
 def f(x):
     # Change this return command to your desired function
-    return (x**5 - 3*x + 5)
+    return ((x**2) - 1)
 
+def g(x):
+    return (math.exp(1)**(1 - (x**2)))
 
 def bisection(a, b, e, n):
     i = 1
     fa = f(a)
-    fb = f(b)
-    if (fa * fb > 0):
-        print("Not possible to guarantee a solution.")
-        return
-
+    
     while (i <= n):
         p = a + ((b-a)/2)
         fp = f(p)
 
-        if (fp == 0 or ((b-a)/2) < e):
+        if (fp == g(p) or ((b-a)/2) < e):
             print("f(p) = " + str(fp))
             print("Number of iterations required to terminate: " + str(i))
             return p
 
         i += 1
 
-        if (fa * fp > 0):
+        if ((fa > g(a) and fp < g(p)) or (fa < g(a) and fp > g(p))):
+            b = p
+        else:
             a = p
             fa = fp
-        else:
-            b = p
 
     print("Failure - Max Iterations Reached")
     return
-
-
-# print("Test 1.a.")
-# print("Estimated p = " + str(bisection(1, 2, 0.001, 100)))
-
-# print("Test 1.b.")
-# print("Estimated p = " + str(bisection(-1, 4, 0.001, 100)))
-
-# print("Test 1.c.")
-# print("Estimated p = " + str(bisection(-0.5, 1, 0.001, 8)))
-
-# print("Test 2.a.")
-# print("Estimated p = " + str(bisection(2.7, 3.5, 0.001, 100)))
-
-# print("Test 2.b.")
-# print("Estimated p = " + str(bisection(-4, -1, 0.001, 20)))
 
 while (True):
     a = float(input("Enter a value for a. "))
