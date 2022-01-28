8126cbfaef8a879805c62a509753e196e342a22d
Stochastis <59930635+Stochastis@users.noreply.github.com>
2022-01-28 09:47:58 -0600

    Proper success output

--- Program 2 - Newton's Method.py
+++ Program 2 - Newton's Method.py
@@ -1,20 +1,26 @@
 def f(x):
-    return x
+    return x**3 + 4*x**2 - 10
 
 
 def ff(x):
-    return x
+    return 3*x**2 + 8*x
 
-def Newtons(p0,e,n0):
+
+def Newtons(p0, e, n0):
     i = 1
     while (i <= n0):
         p = p0 - f(p0)/ff(p0)
         if abs(p - p0) < e:
-            return p
+            print("Approximation of p: " + str(p))
+            print("Approximation of pn-1 " + str(p0))
+            print("f(pn) = " + str(f(p0)))
+            print("Number of iterations: " + str(i))
+            return
         i += 1
         p0 = p
     print("Failure - Max Iterations Reached")
     return
+
 
 p0 = float(input("Please enter your initial guess."))
 e = float(input("Please enter your desired tolerance."))
@@ -24,3 +30,5 @@ while (e <= 0):
     e = float(input("Please enter a positive value for tolerance."))
 while (n0 <= 0):
     n0 = int(input("Please enter a positive number of iterations."))
+
+Newtons(p0, e, n0)
