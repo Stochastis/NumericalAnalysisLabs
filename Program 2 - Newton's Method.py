3c853cc2654ff218a7cc1d6709f35e42f9c405ab
Stochastis <59930635+Stochastis@users.noreply.github.com>
2022-01-28 09:39:54 -0600

    Skeleton Code

--- Program 2 - Newton's Method.py
+++ Program 2 - Newton's Method.py
@@ -1,1 +1,26 @@
-print("Hello, World!")
+def f(x):
+    return x
+
+
+def ff(x):
+    return x
+
+def Newtons(p0,e,n0):
+    i = 1
+    while (i <= n0):
+        p = p0 - f(p0)/ff(p0)
+        if abs(p - p0) < e:
+            return p
+        i += 1
+        p0 = p
+    print("Failure - Max Iterations Reached")
+    return
+
+p0 = float(input("Please enter your initial guess."))
+e = float(input("Please enter your desired tolerance."))
+n0 = int(input("Please enter the maximum number of iterations."))\
+
+while (e <= 0):
+    e = float(input("Please enter a positive value for tolerance."))
+while (n0 <= 0):
+    n0 = int(input("Please enter a positive number of iterations."))
