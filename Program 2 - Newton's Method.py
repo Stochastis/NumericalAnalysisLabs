763e640f651e13433f3c453ac2adb1a1ba3398cd
Stochastis <59930635+Stochastis@users.noreply.github.com>
2022-01-31 09:49:55 -0600

    Prep for test cases

--- Program 2 - Newton's Method.py
+++ Program 2 - Newton's Method.py
@@ -1,36 +1,44 @@
 def f(x):
-    return x**3 + 4*x**2 - 10
+    return x**2 + 1
 
 
 def ff(x):
-    return 3*x**2 + 8*x
+    return 2*x
 
 
 def Newtons(p0, e, n0):
     i = 1
+    p = p0
     while (i <= n0):
-        p = p0 - f(p0)/ff(p0)
+        p0 = p
+        p = p0 - (f(p0)/ff(p0))
         if abs(p - p0) < e:
             print("Approximation of p: " + str(p))
             print("Approximation of pn-1 " + str(p0))
             print("f(pn) = " + str(f(p0)))
-            print("Number of iterations: " + str(i))
+            print("Number of iterations: " + str(i) + "\n")
             return
         i += 1
-        p0 = p
     print("Failure - Max Iterations Reached")
     print("Approximation of p: " + str(p))
-    print("Approximation of pn-1 " + str(p0))
-    print("f(pn) = " + str(f(p0)))
+    print("Approximation of pn-1: " + str(p0))
+    print("f(pn) = " + str(f(p)) + "\n")
 
 
-p0 = float(input("Please enter your initial guess."))
-e = float(input("Please enter your desired tolerance."))
-n0 = int(input("Please enter the maximum number of iterations."))\
+# Test cases
+print("Test 1.a\n")
+Newtons(0.5, 0.0001, 10)
+print("Test 1.b\n")
+Newtons(1.4, 0.0001, 8)
 
-while (e <= 0):
-    e = float(input("Please enter a positive value for tolerance."))
-while (n0 <= 0):
-    n0 = int(input("Please enter a positive number of iterations."))
+while(True):
+    p0 = float(input("Please enter your initial guess. "))
+    e = float(input("Please enter your desired tolerance. "))
+    n0 = int(input("Please enter the maximum number of iterations. "))\
 
-Newtons(p0, e, n0)
+    while (e <= 0):
+        e = float(input("Please enter a positive value for tolerance. "))
+    while (n0 <= 0):
+        n0 = int(input("Please enter a positive number of iterations. "))
+
+    Newtons(p0, e, n0)
