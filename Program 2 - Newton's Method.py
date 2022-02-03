14a5cb651d42ac7a4276b207df89d04892836efb
Stochastis <59930635+Stochastis@users.noreply.github.com>
2022-02-02 20:44:28 -0600

    Completion of all test cases

--- Program 2 - Newton's Method.py
+++ Program 2 - Newton's Method.py
@@ -1,9 +1,4 @@
-def f(x):
-    return x**2 + 1
-
-
-def ff(x):
-    return 2*x
+import math
 
 
 def Newtons(p0, e, n0):
@@ -15,7 +10,7 @@ def Newtons(p0, e, n0):
         if abs(p - p0) < e:
             print("Approximation of p: " + str(p))
             print("Approximation of pn-1 " + str(p0))
-            print("f(pn) = " + str(f(p0)))
+            print("f(pn) = " + str(f(p)))
             print("Number of iterations: " + str(i) + "\n")
             return
         i += 1
@@ -25,20 +20,73 @@ def Newtons(p0, e, n0):
     print("f(pn) = " + str(f(p)) + "\n")
 
 
+def Secant(p0, p1, e, n0):
+    i = 2
+    fp0 = f(p0)
+    fp1 = f(p1)
+    while (i <= n0):
+        p = p1 - ((fp1 * (p1 - p0)) / (fp1 - fp0))
+        if (abs(p - p1) < e):
+            print("Approximation of p: " + str(p))
+            print("f(pn) = " + str(f(p)))
+            print("Number of iterations: " + str(i) + "\n")
+            return
+        i += 1
+        p0 = p1
+        fp0 = fp1
+        p1 = p
+        fp1 = f(p)
+    print("Failure - Max Iterations Reached")
+    print("Approximation of p: " + str(p))
+    print("f(pn) = " + str(f(p)) + "\n")
+
+
 # Test cases
 print("Test 1.a\n")
+def f(x): return x**2 + 1
+def ff(x): return 2*x
+
+
 Newtons(0.5, 0.0001, 10)
+
 print("Test 1.b\n")
+def f(x): return math.atan(x)
+def ff(x): return 1 / (1 + x**2)
+
+
 Newtons(1.4, 0.0001, 8)
 
-while(True):
-    p0 = float(input("Please enter your initial guess. "))
-    e = float(input("Please enter your desired tolerance. "))
-    n0 = int(input("Please enter the maximum number of iterations. "))\
+print("Test 2.a\n")
+def f(x): return (1 / (1 + x**2)) - 1/2
+def ff(x): return -((2*x) / ((x**2 + 1)**2))
 
-    while (e <= 0):
-        e = float(input("Please enter a positive value for tolerance. "))
-    while (n0 <= 0):
-        n0 = int(input("Please enter a positive number of iterations. "))
 
-    Newtons(p0, e, n0)
+Newtons(2, 0.001, 10)
+
+print("Test 3.a\n")
+def f(x): return 5*x + 7
+def ff(x): return 5
+
+
+Newtons(1000, 0.0001, 100)
+
+print("Test 3.b\n")
+def f(x): return x**3 + 4*x**2 - 10
+def ff(x): return 3*x**2 + 8*x
+
+
+Newtons(1, 0.0001, 20)
+
+print("Test 4.a\n")
+def f(x): return x**3 + 4*x**2 - 10
+def ff(x): return 3*x**2 + 8*x
+
+
+Secant(1, 2, 0.0001, 20)
+
+print("Test 4.a\n")
+def f(x): return math.cos(x) - x
+def ff(x): return -(math.sin(x)) - 1
+
+
+Secant(0.5, math.pi/4, 0.0001, 20)
