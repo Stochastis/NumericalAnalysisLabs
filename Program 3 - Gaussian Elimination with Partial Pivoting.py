0d5db65906d4f9b66af4107ec01a0bf5b5698b61
Stochastis <59930635+Stochastis@users.noreply.github.com>
2022-02-14 09:39:23 -0600

    Skeleton code

---
+++ Program 3 - Gaussian Elimination with Partial Pivoting.py
@@ -0,0 +1,27 @@
+def switchRows(indices, index1, index2):
+    temp = index1
+    indices[index1] = index2
+    indices[index2] = temp
+    print("Switched rows " + str(index1) + " and " + str(index2) + ".")
+    return indices
+
+
+def multiplyAndAdd(matrix, indices, rowToAddTo, rowBeingAdded, scalar):
+    scaledRow = [i * scalar for i in matrix[indices[rowBeingAdded]]]
+    for i in range(len(matrix[indices[rowToAddTo]])):
+        matrix[indices[rowToAddTo]][i] += scaledRow[i]
+    print("Added %i * row %i to row %i." % (scalar, rowBeingAdded, rowToAddTo))
+    return matrix
+
+
+def printMatrix(matrix, indices):
+    for index in indices:
+        print(matrix[index])
+    print('')
+
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+indices = [0, 1, 2]
+printMatrix(matrix, indices)
+multiplyAndAdd(matrix, indices, 1, 2, 4)
+printMatrix(matrix, indices)
