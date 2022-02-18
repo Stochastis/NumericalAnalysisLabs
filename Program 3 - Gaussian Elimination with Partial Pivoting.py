0fbd43206b264f27d992094b7e45ba391a81b233
Stochastis <59930635+Stochastis@users.noreply.github.com>
2022-02-18 16:57:17 -0600

    Code draft completed

--- Program 3 - Gaussian Elimination with Partial Pivoting.py
+++ Program 3 - Gaussian Elimination with Partial Pivoting.py
@@ -1,6 +1,6 @@
 def switchRows(indices, index1, index2):
     temp = indices[index1]
-    indices[index1] = index2
+    indices[index1] = indices[index2]
     indices[index2] = temp
     print("Switched rows %i and %i." % (index1, index2))
     printMatrix(matrix, indices)
@@ -31,6 +31,7 @@ def printMatrix(matrix, indices):
 
 
 def GPP(matrix, indices, n):
+    solutions = [0]*(n-1)
     # Elimination Process
     for i in range(n - 1):
         p = bestFirstRow(matrix, indices, i, n)
@@ -46,12 +47,23 @@ def GPP(matrix, indices, n):
     if matrix[indices[n-1]][n-1] == 0:
         print("No unique solution exists.")
         return
-    pass
+    # Xn = right side of equation divided by coefficient
+    solutions[-1] = matrix[indices[-1]][-1] / matrix[indices[-1]][-2]
+    for i in range(n-1, 0, -1):
+        sum = 0
+        for j in range(i+1, n):
+            sum += matrix[indices[i]][j] * solutions[j]
+        solutions[i] = (matrix[indices[-1]][i] - sum) / matrix[indices[i]][i]
+    return solutions
 
 
 # Manually enter your data here
-matrix = [[1, 6, 3], [4, 5, 6], [7, 0, 9]]
-indices = [0, 1, 2]
-n = 3
+# Augmented matrix
+matrix = [[1, 1, 1, 3], [1, 2, 3, 0], [1, 3, 2, 3]]
+
+n = len(matrix)
+indices = [0]*n
+for i in range(n):
+    indices[i] = i
 printMatrix(matrix, indices)
-GPP(matrix, indices, n)
+print(GPP(matrix, indices, n))
