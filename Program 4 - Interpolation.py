a0b532afb21b3449682af324558c36eb1cf491cf
Stochastis <59930635+Stochastis@users.noreply.github.com>
2022-03-04 09:51:23 -0600

    NewtonDDF Complete

--- Program 4 - Interpolation.py
+++ Program 4 - Interpolation.py
@@ -3,11 +3,10 @@ import math
 
 def NewtonDDF(xPoints, yPoints):
     n = len(xPoints)-1
-    F = [[0]*(n+1)]*(n+1)
-    printF(F)
+    f = [[0]*(n+1)for i in range(n+1)]
 
-    F[0] = yPoints
-    printF(F)
+    for i in range(n+1):
+        f[i][0] = yPoints[i]
 
     # Error checking
     if len(xPoints) != len(yPoints):
@@ -16,11 +15,14 @@ def NewtonDDF(xPoints, yPoints):
 
     for i in range(1, n+1):
         for j in (range(1, i+1)):
-            F[i][j] = (F[i][j-1] - F[i-1][j-1]) / (xPoints[i] - xPoints[i-j])
+            f[i][j] = (f[i][j-1] - f[i-1][j-1]) / (xPoints[i] - xPoints[i-j])
+
+    printF(f)
+    print("")
 
     result = [0]*(n+1)
     for i in range(n+1):
-        result[i] = F[i][i]
+        result[i] = f[i][i]
     return result
 
 
@@ -29,7 +31,17 @@ def printF(F):
         print(row)
 
 
-xPoints = [0, 1, 2, 3]
-yPoints = [1, math.e, math.e**2, math.e**3]
+def estimate(xPoint, coefficients, xPoints):
+    factor = 1
+    result = 0
+    for i in range(len(coefficients)):
+        result += coefficients[i] * factor
+        factor *= (xPoint - xPoints[i])
+    return "P(%f) = %f" % (xPoint, result)
 
-print(NewtonDDF(xPoints, yPoints))
+
+xPoints = [1.0, 1.3, 1.6, 1.9, 2.2]
+yPoints = [0.7651977, 0.6200860, 0.4554022, 0.2818186, 0.1103623]
+coefficients = NewtonDDF(xPoints, yPoints)
+print(str(coefficients) + "\n")
+print(estimate(1.5, coefficients, xPoints))
