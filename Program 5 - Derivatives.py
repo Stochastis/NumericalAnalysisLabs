4beb942aac98000c65d0e24388e52427a67dcae7
Stochastis <59930635+Stochastis@users.noreply.github.com>
2022-03-30 09:51:13 -0500

    Completed two point

--- Program 5 - Derivatives.py
+++ Program 5 - Derivatives.py
@@ -4,13 +4,15 @@ def twoPointDerivative(xPoints, yPoints):
     and lagrange interpolating polynomials.
     """
 
-    twoPoints = []
+    twoPoints = [0, 0]
     h = xPoints[1] - xPoints[0]
 
     twoPoints[0] = (yPoints[1] - yPoints[0]) / h
+    twoPoints[1] = (yPoints[-1] - yPoints[-2]) / h
 
     return twoPoints
 
 
 xPoints = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 yPoints = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+print(twoPointDerivative(xPoints, yPoints))
