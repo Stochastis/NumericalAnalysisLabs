d6660325fcc96a612b886b7b57950ef3d558e96d
Stochastis <59930635+Stochastis@users.noreply.github.com>
2022-04-08 09:52:25 -0500

    Continuing derivatives

--- Program 5 - Derivatives.py
+++ Program 5 - Derivatives.py
@@ -4,13 +4,36 @@ def twoPointDerivative(xPoints, yPoints):
     and lagrange interpolating polynomials.
     """
 
-    twoPoints = [0, 0]
+    results = [0, 0]
     h = xPoints[1] - xPoints[0]
 
-    twoPoints[0] = (yPoints[1] - yPoints[0]) / h
-    twoPoints[1] = (yPoints[-1] - yPoints[-2]) / h
+    results[0] = (yPoints[1] - yPoints[0]) / h
+    results[1] = (yPoints[-1] - yPoints[-2]) / h
 
-    return twoPoints
+    return results
+
+
+def threePointDerivative(xPoints, yPoints):
+    """
+    For the second-from-left and second-from-right points,
+    the three-point midpoint formula is used."""
+
+    results = [0, 0]
+    h = xPoints[1] - xPoints[0]
+
+    results[0] = (yPoints[2] - yPoints[0]) / (2*h)
+    results[1] = (yPoints[-1] - yPoints[-3]) / (2*h)
+
+    return
+
+
+def fivePointDerivative(xPoints, yPoints, pointIndex):
+    """
+    For all interior points, use the five-point midpoint formula
+    using the two closest points on either side."""
+
+    h = xPoints[1] - xPoints[0]
+    return
 
 
 xPoints = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
